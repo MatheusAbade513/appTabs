0a769877934d5003c3ecc96dfcec001359cb6631
MatheusAbade513 <matheusabade2@gmail.com>
2023-12-13 16:56:33 -0300

    appTabs

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,16 +1,31 @@
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss']
 })
 export class Tab1Page {
-  likes:number=0
-  addLikes(){
-    this.likes++
+  showForm: boolean = false;
+  email: string = '';
+  password: string = '';
+
+  openForm() {
+    this.showForm = !this.showForm;
   }
 
-  constructor() {}
+  closeForm() {
+    this.showForm = false;
+  }
 
+  submitForm() {
+    // Handle login logic here using this.email and this.password
+    console.log('Login submitted:', this.email, this.password);
+  }
+
+  register() {
+    // Handle registration logic here using this.email and this.password
+    console.log('Registration submitted:', this.email, this.password);
+  }
 }
